4c8483e243d52cb57b2fa4989f851b7b822d1598
BobyCloudlet <vakada13@gmail.com>
2023-11-03 21:58:21 +0300

    StudieXAML

---
+++ StudieXAML/View/UserControls/ModernTextBox/ModernTextBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StudieXAML.View.UserControls.ModernTextBox
+{
+    public partial class ModernTextBox : UserControl
+    {
+        public ModernTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            {   placeholder = value; 
+                tbPlaceholder.Text = placeholder;
+            } 
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(input.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
